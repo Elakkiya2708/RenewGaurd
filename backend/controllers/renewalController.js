19862e1c93080ded7b257bbc954bd31427d18783
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 22:07:17 +0530

    update

--- backend/controllers/renewalController.js
+++ backend/controllers/renewalController.js
@@ -1,5 +1,5 @@
 const supabase = require("../config/database");
-
+const { createDefaultReminders } = require("../services/reminderService");
 exports.getRenewals = async (req, res) => {
     try {
         const { data, error } = await supabase
