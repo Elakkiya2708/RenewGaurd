8606327f31705cd01d6dc080da9add2b334b0447
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 14:57:28 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
     res.send("RenewGuard API is running");
 });
