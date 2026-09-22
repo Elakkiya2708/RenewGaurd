82565aa7082a7f5309a8ef79004e78303479132c
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 18:47:46 +0530

    update

--- backend/routes/renewalRoutes.js
+++ backend/routes/renewalRoutes.js
@@ -2,13 +2,16 @@ const express = require("express");
 
 const {
     getRenewals,
-    addRenewal
+    addRenewal,
+    updateRenewal,
+    deleteRenewal
 } = require("../controllers/renewalController");
 
 const router = express.Router();
 
 router.get("/", getRenewals);
-
 router.post("/", addRenewal);
+router.put("/:id", updateRenewal);
+router.delete("/:id", deleteRenewal);
 
 module.exports = router;
