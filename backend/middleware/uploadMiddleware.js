04b3d462d6b3a081dd2c585e681f612be90fd94e
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 18:57:32 +0530

    update

---
+++ backend/middleware/uploadMiddleware.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "../uploads");
+    },
+
+    filename: (req, file, cb) => {
+        const name = Date.now() + "-" + file.originalname;
+        cb(null, name);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 10 * 1024 * 1024
+    }
+});
+
+module.exports = upload;
