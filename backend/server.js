48f8e569dd221fd15d091eb8a3161f2c12437fff
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 22:29:01 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const documentRoutes = require("./routes/documentRoutes");
 const reminderRoutes = require("./routes/reminderRoutes");
+
 require("dotenv").config();
 
 const app = express();
