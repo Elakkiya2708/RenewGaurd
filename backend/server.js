442d8fe1e34f60154718b56640cde23f68f9a33c
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 18:36:20 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const renewalRoutes = require("./routes/renewalRoutes");
+app.use("/api/renewals", renewalRoutes);
 // Routes
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
