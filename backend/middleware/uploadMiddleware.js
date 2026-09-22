ddcd5745f3554e92c8ddd5fa1d67179d2b7e0892
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-22 19:19:20 +0530

    update

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,18 +1,25 @@
 const multer = require("multer");
+const path = require("path");
+
+const uploadPath = path.join(__dirname, "../../uploads");
 
 const storage = multer.diskStorage({
+
     destination: (req, file, cb) => {
-        cb(null, "../uploads");
+        cb(null, uploadPath);
     },
 
     filename: (req, file, cb) => {
-        const name = Date.now() + "-" + file.originalname;
+        const name =
+            Date.now() + "-" + file.originalname;
+
         cb(null, name);
     }
 });
 
 const upload = multer({
     storage: storage,
+
     limits: {
         fileSize: 10 * 1024 * 1024
     }
