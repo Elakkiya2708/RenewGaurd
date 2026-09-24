7de2304e5bc82da6700ffec24469e56b648960d9
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 08:54:06 +0530

    update the fuiles

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const documentRoutes = require("./routes/documentRoutes");
-const reminderRoutes = require("./routes/reminderRoutes");
-
 require("dotenv").config();
 
 const app = express();
 
-// Middleware
-app.use("/api/reminders", reminderRoutes);
 app.use(cors({
-    origin: true
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"]
 }));
-app.use(express.json());
-app.use("/api/documents", documentRoutes);
-const renewalRoutes = require("./routes/renewalRoutes");
-app.use("/api/renewals", renewalRoutes);
-// Routes
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
 
-// Test route
+app.use(express.json());
+
+const authRoutes = require("./routes/authRoutes");
+const renewalRoutes = require("./routes/renewalRoutes");
+const documentRoutes = require("./routes/documentRoutes");
+const reminderRoutes = require("./routes/reminderRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/renewals", renewalRoutes);
+app.use("/api/documents", documentRoutes);
+app.use("/api/reminders", reminderRoutes);
+
 app.get("/", (req, res) => {
     res.json({
         message: "RenewGuard API is running"
     });
 });
 
-// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
