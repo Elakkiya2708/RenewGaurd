cb71182cf7400202c9904a4406799adff2d5fd35
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 10:57:08 +0530

    update the files

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -1,5 +1,5 @@
 const supabase = require("../config/database");
-
+const { createPDF } = require("../services/reportService");
 exports.getReport = async (req, res) => {
     try {
         const { data, error } = await supabase
