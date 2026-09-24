d35c6e785888632882811b018c86f66bd89ca1bd
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 14:22:27 +0530

    update the files

--- backend/routes/auditRoutes.js
+++ backend/routes/auditRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
-
-const {
-    getAuditLogs
-} = require("../controllers/auditController");
-
+const { getAuditLogs } = require("../controllers/auditController");
 const authMiddleware = require("../middleware/authMiddleware");
+const adminOnly = require("../middleware/roleMiddleware");
 
 const router = express.Router();
 
-router.get("/", authMiddleware, getAuditLogs);
+router.get(
+    "/",
+    authMiddleware,
+    adminOnly,
+    getAuditLogs
+);
 
 module.exports = router;
