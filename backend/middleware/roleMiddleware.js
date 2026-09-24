73034d432ddd06531e73ba26efc09f3febe93b30
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 11:19:25 +0530

    update the files

---
+++ backend/middleware/roleMiddleware.js
@@ -0,0 +1,18 @@
+function adminOnly(req, res, next) {
+
+    if (!req.user) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        });
+    }
+
+    if (req.user.role !== "Admin") {
+        return res.status(403).json({
+            message: "Admin access required"
+        });
+    }
+
+    next();
+}
+
+module.exports = adminOnly;
