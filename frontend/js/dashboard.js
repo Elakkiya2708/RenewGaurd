c14448cca14293d5d67d797176934264b12ed4b7
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 15:40:13 +0530

    update the files

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -222,3 +222,24 @@ document
 
 // Start
 loadDashboard();
+
+const themeToggle = document.getElementById("themeToggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+    document.body.classList.add("light-theme");
+    themeToggle.textContent = "🌙";
+} else {
+    themeToggle.textContent = "☀️";
+}
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("light-theme");
+
+    const isLight = document.body.classList.contains("light-theme");
+
+    localStorage.setItem("theme", isLight ? "light" : "dark");
+
+    themeToggle.textContent = isLight ? "🌙" : "☀️";
+});
