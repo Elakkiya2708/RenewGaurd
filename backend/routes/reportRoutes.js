deff599d2b18ea35341b45f2648f3b40b1b76395
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 10:46:38 +0530

    update the files

---
+++ backend/routes/reportRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {
+    getReport
+} = require("../controllers/reportController");
+
+const router = express.Router();
+
+router.get("/", getReport);
+
+module.exports = router;
