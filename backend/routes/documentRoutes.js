cef6f467211408800249f1d965cd64b6f0188da1
Elakkiya Varadharajan <elakkiyaohai@gmail.com>
2026-09-24 17:39:01 +0530

    update the files

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -1,19 +1,50 @@
 const express = require("express");
 
 const upload = require("../middleware/uploadMiddleware");
+
 const authMiddleware = require("../middleware/authMiddleware");
 
 const {
-    uploadDocument
+    uploadDocument,
+    getDocuments,
+    downloadDocument,
+    deleteDocument
 } = require("../controllers/documentController");
 
 const router = express.Router();
 
+
+// Upload
 router.post(
     "/",
     authMiddleware,
     upload.single("file"),
     uploadDocument
 );
+
+
+// Get all documents
+router.get(
+    "/",
+    authMiddleware,
+    getDocuments
+);
+
+
+// Download
+router.get(
+    "/download/:id",
+    authMiddleware,
+    downloadDocument
+);
+
+
+// Delete
+router.delete(
+    "/:id",
+    authMiddleware,
+    deleteDocument
+);
+
 
 module.exports = router;
